5afe1cc1c7a9cfcafad4e7f8f606391c5945886e
ycatan-w <225661717+ycatan-w@users.noreply.github.com>
2026-06-10 22:25:27 +0200

    use createWebHashHistory + readd package-lock

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import { routes } from './routes.ts'
 import { registerGuards } from './guards.ts'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
